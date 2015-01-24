4943e3d87af8d9df03dca9acbd136cd43b6e1507
Alex Gor <ifresh.wp@gmail.com>
2015-01-24 11:32:47 +0400

    Initial commit

---
+++ prj/main.cpp
@@ -0,0 +1,26 @@
+#define WIN32_LEAN_AND_MEAN           
+#include <windows.h>
+
+#include <tchar.h>
+
+// C RunTime Header Files
+#include "platform/CCStdC.h"
+#include "platform/CCPlatformMacros.h"
+#include "platform/CCPlatformDefine.h"
+
+#include "appdelegate.h"
+
+USING_NS_CC;
+
+int APIENTRY _tWinMain(HINSTANCE hInstance,
+    HINSTANCE hPrevInstance,
+    LPTSTR    lpCmdLine,
+    int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    // create the application instance
+    AppDelegate app;
+    return Application::getInstance()->run();
+}
